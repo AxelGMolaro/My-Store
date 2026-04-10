e14a21d6da720bda9a58b50cb7bb1c76f432db14
axelmolaro <axel@blockinar.com>
2026-04-10 12:08:37 -0300

    custom pipe

--- src/app/components/product.component/product.component.ts
+++ src/app/components/product.component/product.component.ts
@@ -2,10 +2,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { ImgComponent } from '../img/img.component';
 import { CommonModule } from '@angular/common';
+import { ReversePipe } from '../../pipes/reverse.pipe';
 
 @Component({
   selector: 'app-product',
-  imports: [ImgComponent,CommonModule],
+  imports: [ImgComponent,CommonModule, ReversePipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
   standalone: true
