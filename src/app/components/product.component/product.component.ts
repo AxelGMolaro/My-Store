ad9bed16b485fd41f87dbaf2e15a5516c7814378
axelmolaro <axel@blockinar.com>
2026-04-10 10:51:01 -0300

    fin del modulo

--- src/app/components/product.component/product.component.ts
+++ src/app/components/product.component/product.component.ts
@@ -1,14 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { ImgComponent } from '../img/img.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
-  imports: [ImgComponent],
+  imports: [ImgComponent,CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
 export class ProductComponent {
 
   @Input() product!: Product;
+  @Output() addedProduct = new EventEmitter<Product>()
+
+  onAddToCart(){
+    this.addedProduct.emit(this.product)
+  }
 }
--- src/app/pages/home.page/home.page.ts
+++ src/app/pages/home.page/home.page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ProductsComponent } from '../../components/products.component/products.component';
+import { NavComponent } from '../../components/nav.component/nav.component';
 
 @Component({
   selector: 'app-home.page',
-  imports: [ProductsComponent],
+  imports: [ProductsComponent,NavComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
 })
