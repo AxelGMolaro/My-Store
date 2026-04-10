a2cec5fcaf4a69188f698198595d786366e363bc
axelmolaro <axel@blockinar.com>
2026-04-10 13:08:27 -0300

    reactivo

--- src/app/components/nav.component/nav.component.ts
+++ src/app/components/nav.component/nav.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { StoreService } from '../../services/store.service';
 @Component({
   selector: 'app-nav',
   imports: [],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
    activeMenu = false;
+   counter = 0;
+
+   constructor(
+    private storeService: StoreService
+   ){}
+
+   ngOnInit(): void {
+     this.storeService.myCart$.subscribe(
+      products => {
+        this.counter = products.length
+      }
+     )
+   }
 
    toggleMenu() {
     this.activeMenu = !this.activeMenu;
